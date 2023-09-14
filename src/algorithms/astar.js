9d0dc0d52c890326f85adc4e671823c2568fb786
Stepan Susorov <137768644+Ba6ySHark@users.noreply.github.com>
2023-09-13 21:51:17 -0600

    completed A*

--- src/algorithms/astar.js
+++ src/algorithms/astar.js
@@ -4,9 +4,86 @@ function manhattanDistance(currentNode, goalNode) {
     return Math.abs(currentNode.row - goalNode.row) + Math.abs(currentNode.column - goalNode.column);
 }
 
+function getUnvisitedNeighbors(node, list) {
+    const res = [];
+    const {row, column} = node;
+
+    if (row > 0) {
+        res.push(list[row - 1][column]);
+    };
+
+    if (row < list.length - 1) {
+        res.push(list[row + 1][column]);
+    };
+
+    if (column > 0) {
+        res.push(list[row][column - 1]);
+    };
+
+    if (column < list[0].length - 1) {
+        res.push(list[row][column + 1]);
+    };
+    
+    return res.filter((node) => !node.isWall);
+}
+
+function isInOpenSet(node, openSet) {
+    for (const element of openSet) {
+        if ((node.row == element.row) && (node.column == element.column) && (node.total_cost >= element.total_cost)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isInClosedSet(node, closedSet) {
+    for (const element of closedSet) {
+        if ((node.row == element.row) && (node.column == element.column) && (node.total_cost >= element.total_cost)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 export default function astarAlgorithm(nodes, startNode, endNode) {
-    const openSet = new Set([startNode]);
-    const cameFrom = new Map();
+    startNode.f_cost = 0;
+    startNode.g_cost = 0;
+    startNode.total_cost = 0;
+    const openSet = [startNode];
+    const closedSet = [];
+    const path = [];
 
+    while (openSet.length > 0) {
+        console.log(openSet);
+        openSet.sort((firstNode, secondNode) => (firstNode.total_cost - secondNode.total_cost));
+        let current = openSet.shift();
+        let childNodes = getUnvisitedNeighbors(current, nodes);
 
+        for (const child of childNodes) {
+            if (child == endNode) {
+                closedSet.push(current);
+                return closedSet;
+            }
+            else {
+                //child.isMarked = true;
+                child.f_cost = current.f_cost + 0.25;
+                child.g_cost = manhattanDistance(child, endNode);
+                child.total_cost = child.f_cost + child.g_cost;
+
+                if (isInOpenSet(child, openSet)) {
+                    continue;
+                }
+
+                if (isInClosedSet(child, closedSet)) {
+                    continue;
+                }
+                else {
+                    openSet.push(child);
+                    path.push(child);
+                }
+            }
+        }
+
+        closedSet.push(current);
+    }
 }
